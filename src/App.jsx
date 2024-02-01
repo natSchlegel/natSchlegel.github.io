1332867c1e407ec31950c25060a25f8a31359880
Natascha <nataschaschlegel27@gmail.com>
2024-02-01 11:24:42 +0100

    react

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="container-1"></div>
+        <div className="container-2"></div>
+        <div className="container-3"></div>
+        <div className="container-big">
+          <div className="container-big-div">
+            <div className="container-big-div-div">
+              <div className="container-big-div-div-circles-div">
+                <div className="circle-red"></div>
+                <div className="circle-yellow"></div>
+                <div className="circle-green"></div>
+              </div>
+              <div className="tab-1">dsf</div>
+              <div className="tab-2">dsf</div>
+            </div>
+            <div className="url-div">
+              <div className="url-div-div">
+                <span className="url-text">
+                  https://natschlegel.github.io/curriculo
+                </span>
+                {/* <span className="material-symbols-outlined" style="color: #f2cdc0;">arrow_back_ios</span>
+                <span className="material-symbols-outlined" style="color: #f2cdc0;">arrow_forward_ios</span>
+                <span className="material-symbols-outlined" style="color: #f2cdc0;">refresh</span> */}
+              </div>
+            </div>
+              <div className="teste-2">
+                <div className="teste-3">
+                  <div class="titulo-interior">Education History</div>Lorem
+                  ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                  pretium leo eget nisl iaculis consectetur. Pellentesque in
+                  accumsan dui, in lacinia dui. Vestibulum nec rutrum leo, quis
+                  laoreet dolor. Sed luctus lacus sit amet tortor rutrum
+                  euismod. Aenean iaculis pellentesque nisl, finibus egestas
+                  neque tincidunt vitae. Donec pretium pulvinar ex quis
+                  ultrices. Aenean fringilla elit pulvinar, efficitur nisi sed,{" "}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    </>
+  );
+}
+
+export default App;
